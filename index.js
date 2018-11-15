723d0b6f3544d7345fb131a27eb432fc57596bca
Sterchois <sterchois@protonmail.com>
2018-11-15 14:44:19 +0800

    finish

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const eventDelegation = (agent, selector, eventName, handler) => {
   agent.addEventListener(eventName, (e) => {
     const target = e.target
     const nodes = document.querySelectorAll(selector)
-    nodes.forEach(node => {
+    for (let i = 0, len = nodes.length; i < len; i++) {
+      const node = nodes[i]
       let tar = target
-      while (tar && tar !== agent) {
+      while (tar && target !== agent) {
         if (node === tar) {
-          return handler(nodes,e,node)
+          return handler(e,node)
         }
         tar = tar.parentNode
       }
-    })
+    }
   })
 }
